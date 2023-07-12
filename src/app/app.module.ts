22c53ed2d7bf9e834d04634688fcec0d379beef0
Mauricio Garnica <20001603@alumnos.utleon.edu.mx>
2023-07-11 19:57:54 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StudentsComponent } from './utl/students/students.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './utl/add/add.component';
+import { EditComponent } from './utl/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    MenuComponent,
+    HomeComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
